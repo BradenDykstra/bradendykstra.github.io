cabfac5c1423a1091565357aa3a17ab3d33e9c78
Braden Dykstra <Toastie285@gmail.com>
2019-11-05 12:46:42 -0700

    page formatting

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'about',
+    component: About
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/projects',
+    name: 'projects',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (projects.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+      return import(/* webpackChunkName: "projects" */ '../views/Projects.vue')
     }
   }
 ]
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -6,6 +6,7 @@
 
 <script>
 // @ is an alias to /src
+
 export default {
   name: "home",
   components: {}
